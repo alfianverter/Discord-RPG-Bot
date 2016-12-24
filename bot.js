355dedf7986c5db8b04a2efbdb065f0094b32193
gotkeyzjr <gotkeyzjr@gmail.com>
2016-12-23 16:22:28 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,13 +32,33 @@ bot.on('message', (msg) => {
               var input = msg.content.toLowerCase();
 //Starts the game and sets the stats.
 if(input === prefix + "start" && isPlaying == false) {
+  //Here is the enemy creation framework.
+  stats.enemies = ['Titan', 'Slime', 'Demon']
+    stats.enemy = stats.enemies[Math.floor(Math.random() * 4)]
+    //Copy this IF statement and add your enemy to the stats.enemies array, then change Slime to your new enemy.
+    //stats.HP is how much health the new enemy will have, and attackMul is multiplies the damage by your number.
+  if(stats.enemy == 'Slime') {
+    stats.attackMul = 1.0
+      stats.HP = 50
+  }
+  if(stats.enemy == 'Titan') {
+    stats.attackMul = 0.5
+      stats.HP = 80
+  }
+  if(stats.enemy == 'Demon') {
+    stats.attackMul = 2.0
+      stats.HP = 35
+  }
+
   round = round + 1
   isPlaying = true
-  stats.HP = 50
+
   stats.plrHP = 40
   stats.maxHP = stats.plrHP
   stats.Mana = 50
-  msg.channel.sendMessage('Slime\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal?')
+
+  stats.onFire = false
+  msg.channel.sendMessage(stats.enemy + '\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal? Fireball?')
 }
 
 //Attacks the foe.
@@ -46,36 +66,64 @@ if(input === "attack" && isPlaying == true) {
   var attackDmg = Math.floor(Math.random() * 15) + 4
   msg.channel.sendMessage('You did ' + attackDmg + ' damage.')
   stats.HP = stats.HP - attackDmg
-  attackDmg = Math.floor(Math.random() * 16) + 3
+  attackDmg = Math.floor(Math.random() * 16) + 3 * stats.attackMul
 stats.plrHP = stats.plrHP - attackDmg
-  msg.channel.sendMessage('Ouch! The slime hit you for ' + attackDmg + ' damage.')
+  msg.channel.sendMessage('Ouch! The ' + stats.enemy + ' hit you for ' + attackDmg + ' damage.')
   round = round + 1
-  msg.channel.sendMessage('Slime\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal?')
+  if(stats.onFire == true && isPlaying == true) {
+    msg.channel.sendMessage('The foe is on fire and lost 2 hitpoints!')
+    stats.HP = stats.HP - 2
+  }
+  msg.channel.sendMessage(stats.enemy + '\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal? Fireball?')
 }
 
 //If you say "heal" and you have enough Mana this code will execute.
 if(input === "heal" && isPlaying == true && stats.Mana > 24) {
   var heal = Math.floor(Math.random() * 13) + 1
   stats.test = heal + stats.plrHP
-if(stats.test > stats.maxHP) {
   stats.plrHP = stats.plrHP + heal
   stats.Mana = stats.Mana - 25
   msg.channel.sendMessage('You healed for ' + heal + ' hitpoints and used 25 mana.')
   msg.channel.sendMessage('The foe didn\'t attack.')
   round = round + 1
-  msg.channel.sendMessage('Slime\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal?')
-} else {
-  msg.channel.sendMessage('You cannot heal over max HP. No Mana was used.')
-  msg.channel.sendMessage('The foe didn\'t attack.')
-  round = round + 1
-  msg.channel.sendMessage('Slime\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal?')
-}
+  if(stats.onFire == true && isPlaying == true) {
+    msg.channel.sendMessage('The foe is on fire and lost 2 hitpoints!')
+    stats.HP = stats.HP - 2
+  }
+  msg.channel.sendMessage(stats.enemy + '\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal? Fireball?')
 
 }
 //If you dont have enough mana this triggers.
 if(input === "heal" && isPlaying == true && stats.Mana < 25) {
 msg.channel.sendMessage('You don\'t have enough Mana!')
 }
+if(input === "fireball" && isPlaying == true && stats.Mana > 5) {
+  var attackDmg = Math.floor(Math.random() * 6) + 2
+
+  stats.Mana = stats.Mana - 3
+  msg.channel.sendMessage('You shot a fireball at the enemy and used 5 mana.')
+  stats.onFire = true
+  attackDmg = Math.floor(Math.random() * 16) + 3 * stats.attackMul
+  stats.plrHP = stats.plrHP - attackDmg
+  msg.channel.sendMessage('Ouch! The ' + stats.enemy + ' hit you for ' + attackDmg + ' damage.')
+  round = round + 1
+  if(stats.onFire == true && isPlaying == true) {
+    msg.channel.sendMessage('The foe is on fire and lost 2 hitpoints!')
+    stats.HP = stats.HP - 5
+  }
+  msg.channel.sendMessage(stats.enemy + '\'s HP: ' + stats.HP + '\nYour HP: ' + stats.plrHP + '\nYour Mana ' + stats.Mana + '\nAttack? Heal? Fireball?')
+
+
+
+}
+
+
+//If you dont have enough mana this triggers.
+if(input === "fireball" && isPlaying == true && stats.Mana < 5) {
+msg.channel.sendMessage('You don\'t have enough Mana!')
+}
+
+
 
 //Evaluation command, if you self-hosting this bot replace my user id with yours.
 if(msg.content.startsWith(prefix + "eval ")) {
